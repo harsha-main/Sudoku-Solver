27b47820660a8345558b2aefecbee1a75cd6f93c
harsha-main <tech_person@yahoo.com>
2017-12-15 21:53:01 +0530

    Initial commit

---
+++ app/src/main/java/com/reallycool/harsha/sudoku/MainActivity.java
@@ -0,0 +1,17 @@
+package com.reallycool.harsha.sudoku;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new Surface(this));
+    }
+}
